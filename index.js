4bfe6f73837d69cde8489bca9e81737a6cb78ad5
Yuri Queiroz <ymendesqueiroz@gmail.com>
2025-04-29 21:59:47 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express"); 
+const app = express(); 
+
+
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo ao meu site de carros!</h1>");
+})
+
+
+app.get("/carros", function(req,res){
+    res.send("<h1>Lista de carros!</h1>");
+})
+
+
+app.get("/consultar/:parametro", function(req,res){
+
+    res.send("retorno da consulta: " + req.params.parametro);
+})
+
+app.listen(process.env.PORT ?? 3000,function(erro){
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado.");
+    }
+})
